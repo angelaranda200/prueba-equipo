d15e0b958b7c4a4766f5f96fabd873a36fa3dcb2
angelaranda200 <angelaranda2011@hotmail.com>
2023-05-06 16:06:00 -0500

    update de holamundo.js

--- src/HolaMundo.js
+++ src/HolaMundo.js
@@ -2,7 +2,11 @@
 
 const HolaMundo = () => {
   return (
+    <>
     <h2>Hola Mundo de parte de Ronny</h2>
+    <h1>Hola Mundo de parte de Angel</h1>
+    </>
+    
   )
 }
  export default HolaMundo
